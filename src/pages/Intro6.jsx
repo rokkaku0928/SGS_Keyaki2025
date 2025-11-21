cb31a9fe665f557123a9cf6377121cc0852e36a8
rokkaku0928 <97389641+rokkaku0928@users.noreply.github.com>
2025-11-21 17:40:14 +0900

    intro文を修正

--- src/pages/Intro6.jsx
+++ src/pages/Intro6.jsx
@@ -4,13 +4,11 @@ export default function Intro6({introStep, setIntroStep }) {
   return (
     <>
       <h1>Intro6</h1>
-      <p>こいつらの中にあるカギは</p>
-      <p>銃のハッキングでしか取れないうえに</p>
-      <p>セキュリティでガッチンガチンに守られてる</p>
+      <p>こいつらの中にあるカギを取るには</p>
+      <p>セキュリティを割らなきゃならん</p>
       <p>取り出しには相応の手間を取ると考えていい。</p>
       <button onClick={() => setIntroStep(introStep + 1)} >つぎへ</button>
       <button onClick={() => setIntroStep(introStep - 1)} >もどる</button>
-          
     </>
   )
 }
--- src/pages/Intro9.jsx
+++ src/pages/Intro9.jsx
@@ -10,7 +10,7 @@ export default function Intro9(props) {
   return (
     <>
         <h1>Are You Ready?</h1>
-        <p>これよりかカメラを起動する</p>
+        <p>これよりカメラを起動する</p>
         <p>ミッションはマークの破壊とセキュリティの突破だ</p>
         <p>残りの時間制限はこちらからカメラ画面に表示する</p>
         <p>諸君の健闘を祈る</p>
