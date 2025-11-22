42af16768589a97f8a296cb5b4d9a1e4057563dd
rokkaku0928 <97389641+rokkaku0928@users.noreply.github.com>
2025-11-22 11:34:33 +0900

    Introを修正

--- src/pages/Intro7.jsx
+++ src/pages/Intro7.jsx
@@ -6,6 +6,10 @@ export default function Intro7({introStep, setIntroStep }) {
         <h1>Intro7</h1>
         <p>セキュリティの</p>
         <p>解除にお前の力を貸してくれ！</p>
+        <p>さて、ここまで読んでいただいてすみませんが</p>
+        <p>銃のトラブルによりQRコードは直接読んでいただきたいです</p>
+        <p>QRコードは教室内にあります</p>
+        <p>ご不便おかけし申し訳ございません</p>
         <button onClick={() => setIntroStep(introStep + 1)} >つぎへ</button>
         <button onClick={() => setIntroStep(introStep - 1)} >もどる</button>
     </>
